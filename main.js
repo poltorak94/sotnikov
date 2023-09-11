d448a40bee5b9500cae0aaeb558142d04a328b20
epol94 <elisey1005@gmail.com>
2023-09-11 19:16:56 +0300

    responsive

--- main.js
+++ main.js
@@ -27,4 +27,25 @@ $(document).ready(function () {
         e.preventDefault();
         $(".cats__table-container").show();
     });
+
+    $(".cats__table-mobile-header-item").on("click", function (e) {
+        e.preventDefault();
+
+        $(".cats__table-mobile-header-item").each(function () {
+            $(this).removeClass("cats__table-mobile-header-item--active");
+        });
+
+        $(this).addClass("cats__table-mobile-header-item--active");
+
+        let navId = $(this).attr("data-id");
+
+        $(".cats__table-mobile-group").each(function () {
+            $(this).removeClass("cats__table-mobile-group--active");
+            let tabId = $(this).attr("data-id");
+
+            if (tabId == navId) {
+                $(this).addClass("cats__table-mobile-group--active");
+            }
+        });
+    });
 });
